08c9e52393aebfddf1be9412bca4b833ed6fa8fc
Javier Montesinos <fj.montesinos@gmail.com>
2020-10-21 00:56:23 +0200

    Updated. Uso en el filtro del endpoint auth del key manager

--- src/main/java/com/montesinos/securedbyheadertoken/server/filters/CheckTokenHeaderFilter.java
+++ src/main/java/com/montesinos/securedbyheadertoken/server/filters/CheckTokenHeaderFilter.java
@@ -1,13 +1,6 @@
 package com.montesinos.securedbyheadertoken.server.filters;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -22,12 +15,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
-import com.montesinos.securedbyheadertoken.server.domain.ApiKey;
 
 @Component
 @Order(1)
@@ -38,25 +32,24 @@ public class CheckTokenHeaderFilter implements Filter {
 	@Value("${apikey.user.name}")
 	private final String apiKeyUserName = null;
 	
+	@Value("${apikey.user.value}")
+	private String apiKeyUserValue;
+		
 	@Value("${apikey.name}")
 	private final String apiKeyName = null;
 	
+	@Value("${apikey.value}")
+	private String apiKeyValue;
+		
 	@Value("${apikey.scope}")
-	private final String apiScope = null;
+	private final String apiScope = null;	
 	
 	@Value("${apikey.manager.url}")
 	private final String apiKeyManagerUrl = null;
 	
-	/**
-	 * Almacena en memoria las keys para dar agilidad a las peticiones
-	 */
-	private Map<String, ApiKey> keys;
-		
 	@Override
 	public void init(final FilterConfig filterConfig) throws ServletException {
-		LOG.info("Initializing filter :{}", this);
-		
-		this.loadKeys();				  
+		LOG.info("Initializing filter :{}", this);						 
 	}	
 
 	@Override
@@ -70,12 +63,15 @@ public class CheckTokenHeaderFilter implements Filter {
 		String userName = req.getHeader(this.apiKeyUserName);
 		
 		LOG.info("req : {}, user name: {}, api key value: {}", req.getRequestURI(), userName, apiKey);
-
-		if(!this.authenticateKey(apiScope, userName, apiKey)) {				
+					
+		if(authApiKey(userName, apiKey) == true) {
+			chain.doFilter(request, response);
+			
+		} else {		
 			res.sendError(HttpStatus.NOT_FOUND.value(), "Recurso no disponible");
-		}
+			
+		}	
 		
-		chain.doFilter(request, response);
 	}
 	
 	@Override
@@ -83,53 +79,23 @@ public class CheckTokenHeaderFilter implements Filter {
 		LOG.warn("Destructing filter :{}", this);
 	}
 	
-	private void loadKeys() {
+	/**
+	 * Valida contra el apikey-manager si el usuario y apikey son correctos
+	 * @param userName
+	 * @param apiKey
+	 * @return
+	 */
+	private boolean authApiKey(String userName, String apiKey) {
 		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<ApiKey[]> response = restTemplate.getForEntity(this.apiKeyManagerUrl, ApiKey[].class);
-		ApiKey[] apiKeys = response.getBody();
 		
-		keys = new HashMap<String, ApiKey>();
-		for(int i = 0; i < apiKeys.length;i++) {
-			keys.put(apiKeys[i].getUsername(), apiKeys[i]);
-		}
-	}
-	
-	private boolean authenticateKey(String apiScope, String userName, String keyUuid) {
-		boolean retValue = false;
-		if(this.keys.containsKey(userName)) {
-			ApiKey keyBD = this.keys.get(userName);
+		HttpHeaders headers = new HttpHeaders();        
+        headers.add(this.apiKeyUserName, this.apiKeyUserValue);
+        headers.add(this.apiKeyName, this.apiKeyValue);
+        
+		String urlAuthenticateKey= this.apiKeyManagerUrl + "/auth/" + this.apiScope + "/" + userName + "/" + apiKey;
+		ResponseEntity<String> keyAuthResponse = restTemplate.exchange(urlAuthenticateKey, 
+				HttpMethod.GET, new HttpEntity<Object>(headers), String.class);		
 
-			MessageDigest md = null;
-			byte[] hashedPassword = null;
-			try {
-				md = MessageDigest.getInstance("SHA-256");				
-				md.update(Base64.getDecoder().decode(keyBD.getSalt().getBytes("UTF-8")));
-				
-				hashedPassword = md.digest(keyUuid.getBytes(StandardCharsets.UTF_8));
-				
-				if(keyBD.getApiScope().equals(apiScope) 
-						&& keyBD.getHashedUuid().equals(byteArrayToHexadecimal(hashedPassword))) {
-					retValue = true;
-				} 
-				
-			} catch (NoSuchAlgorithmException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (UnsupportedEncodingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
-		return retValue;
+		return Boolean.valueOf(keyAuthResponse.getBody()).booleanValue();
 	}
-	
-	private String byteArrayToHexadecimal(byte[] bytes) {
-		StringBuilder sb = new StringBuilder();
-		for(int i=0; i< bytes.length ;i++){
-	        sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-	    }
-		return sb.toString();
-	}
-
 }
